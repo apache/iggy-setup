de39b658877cb902c0cb854eabb8eec431dbbf5c
T1B0 <T1B0@users.noreply.github.com>
2025-08-08 18:59:38 +0200

    Migrate setup iggy (#1)

---
+++ src/functions/download-binary.ts
@@ -0,0 +1,17 @@
+import { info } from "@actions/core";
+import { downloadTool, extractZip } from "@actions/tool-cache";
+
+const arch = "Linux-x86_64";
+
+export async function downloadFile(version: string): Promise<string> {
+  const binaryUrl = version === "latest" ?
+    `https://github.com/apache/iggy/releases/latest/download/${arch}.zip` :
+    `https://github.com/apache/iggy/releases/download/server-${version}/${arch}.zip`;
+
+  info(`Binary downloaded from ${binaryUrl}`);
+
+  const pathToTarball = await downloadTool(binaryUrl);
+  const pathToCLI = await extractZip(pathToTarball);
+
+  return pathToCLI + `/all_artifacts/${arch}`;
+}
---
+++ src/main/index.ts
@@ -0,0 +1,34 @@
+import * as core from "@actions/core";
+
+import { setupBinary } from "./binary";
+import { startIggyServer } from "./start-server";
+
+const DEFAULT_VERSION = "latest";
+
+async function main() {
+  let version = core.getInput("version", { required: false });
+  if (version === null || version === "") {
+    version = DEFAULT_VERSION;
+  }
+  const autoStart = core.getInput("autostart", { required: false });
+
+  try {
+    core.info(`Installing iggy:${version} and adding it to GitHub Actions Path`);
+    await setupBinary(version);
+
+    if (autoStart) {
+      core.info("Starting server...");
+      startIggyServer();
+    }
+  } catch (error) {
+    core.error(
+      `Failed to setup iggy server (version: ${version} autostart: ${autostart})`,
+      error
+    );
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  }
+}
+
+main();
